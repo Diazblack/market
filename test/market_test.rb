65b0bf73401d972ec842d281f6c44d0fd1ca8a02
Cesar Jolibois <diazcd85@gmail.com>
2018-09-12 13:37:44 -0600

    market class create, added vendor method

---
+++ test/market_test.rb
@@ -0,0 +1,45 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/vendor'
+require './lib/market'
+
+
+class MarketTest < Minitest::Test
+  def test_if_exist
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_instance_of Market, market
+  end
+
+  def test_if_it_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_equal "South Pearl Street Farmers Market",market.name
+    assert_equal [], market.vendors
+  end
+
+  def test_if_it_can_add_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+
+    vendor_1.stock("Peaches", 35)
+    vendor_1.stock("Tomatoes", 7)
+
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+
+    vendor_2.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    vendor_3.stock("Peaches", 65)
+
+    market.add_vendor(vendor_1)
+    market.add_vendor(vendor_2)
+    market.add_vendor(vendor_3)
+
+    expected = [vendor_1, vendor_2, vendor_3]
+    assert_equal expected, market.vendors
+  end
+end
