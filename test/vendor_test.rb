b1baca2132ab640b9db37c91c7e94e449d18aa18
Cesar Jolibois <diazcd85@gmail.com>
2018-09-12 13:27:59 -0600

    finish iterarion 1

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -22,4 +22,24 @@ class VendorTest < Minitest::Test
 
     assert_equal 0, vendor.check_stock("Peaches")
   end
+
+  def test_if_it_can_add_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    vendor.stock("Peaches", 30)
+
+    assert_equal 30, vendor.check_stock("Peaches")
+  end
+
+  def test_if_it_add_more_them_one_item
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    vendor.stock("Peaches", 30)
+    vendor.stock("Peaches", 25)
+    vendor.stock("Tomatoes", 12)
+
+    expected ={"Peaches"=>55, "Tomatoes"=>12}
+
+    assert_equal expected, vendor.inventory
+  end
 end
